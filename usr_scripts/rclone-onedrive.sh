bee98e7662ea66cf9214774139b9e5904220477f
matiasgonzalezz0 <matiasgonzalezz.mail@gmail.com>
2024-06-04 20:34:56 -0400

    Added neovim config and changed other minor detailes

---
+++ usr_scripts/rclone-onedrive.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Mounts OneDrive using rclone
+
+mkdir -p ~/OneDrive
+rclone --vfs-cache-mode writes mount OneDrive: ~/OneDrive & 
+notify-send "OneDrive connected" "Microsoft OneDrive successfully mounted."
