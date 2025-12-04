c78a9482e5aa4de8d6b80e9182645ba07a488a06
matiasgonzalezz0 <matiasgonzalezz.mail@gmail.com>
2025-12-04 19:10:11 -0300

    Add: multiple changes

--- config/hypr/launch.sh
+++ config/hypr/launch.sh
@@ -4,8 +4,8 @@
 
 sleep 2s
 
-hyprctl dispatch exec -- [ workspace 17 silent ] ferdium &
-hyprctl dispatch exec -- [ workspace 20 silent ] tidal-hifi --enable-features=UseOzonePlatform --ozone-platform=wayland &
+# hyprctl dispatch exec -- [ workspace 17 silent ] ferdium &
+# hyprctl dispatch exec -- [ workspace 20 silent ] tidal-hifi --enable-features=UseOzonePlatform --ozone-platform=wayland &
 
-radeon-profile &
+# radeon-profile &
 # easyeffects &
